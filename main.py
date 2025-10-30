4eea68caa751c12ea3b2dc5f7d99b12a0b5acc92
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2025-10-30 10:08:11 -0400

    Refactor vanilla into main

--- main.py
+++ main.py
@@ -1,36 +1,84 @@
-import argparse
+import requests
+import json
+import os
 
-from servertypes.fabric import fabric_download
-from servertypes.neoforge import neoforge_download
-from servertypes.paper import paper_download
-from servertypes.purpur import purpur_download
-from servertypes.vanilla import vanilla_download
-from servertypes.forge import forge_download
+def vanilla_download(version):
+    print("Downloading Vanilla " + version)
+    # Vanilla has a json file that contains all version information including server jar download links
+    manifest_url = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+    manifest_json = ""
+
+    try:
+        response = requests.get(manifest_url)
+        response.raise_for_status()
+
+        manifest_json = response.text
+
+        print("Manifest JSON saved successfully")
+    except requests.exceptions.RequestException as e:
+        print(f"Error saving manifest: {e}")
+        return None
+
+    version_url = ""
+    version_json = ""
+
+    # Load the json file as an object and find the selected Minecraft version in the json, then get the URL to that specific version's json file
+    manifest_object = json.loads(manifest_json)
+    for manifest_version in manifest_object["versions"]:
+        if manifest_version["id"] == version:
+            print("Downloading " + manifest_version["url"])
+            version_url = manifest_version["url"]
+
+    try:
+        response = requests.get(version_url)
+        response.raise_for_status()
+
+        version_json = response.text
+
+        print("Version JSON saved successfully")
+    except requests.exceptions.RequestException as e:
+        print(f"Error saving version: {e}")
+        return None
+
+    # Load the version json into an object and then get the server download URL from it
+    version_object = json.loads(version_json)
+
+    version_download_url = version_object["downloads"]["server"]["url"]
+    output_dir = "/"
+    output_file = os.path.join(output_dir, "server.jar")
+    os.makedirs(output_dir, exist_ok=True)
+
+    try:
+        response = requests.get(version_download_url, stream=True)
+        response.raise_for_status()
+
+        print("Downloading Vanilla " + version + " JAR file")
+        with open(output_file, "wb") as file:
+            for chunk in response.iter_content(8192):
+                file.write(chunk)
+        print("Version downloaded successfully to " + output_dir)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading version: {e}")
+        return None
 
 def main():
-    parser = argparse.ArgumentParser(description='Universal Version Downloader')
-
-    parser.add_argument('servertype', type=str, help='Selected Server Type (Vanilla, Fabric, Paper, PurPur, NeoForge, Forge)')
-    parser.add_argument('versions', nargs='+', help='Selected Version or Versions')
-
-    args = parser.parse_args()
-    print("You selected " + args.servertype + " on " + str(args.versions))
-
-    selected_modloader = args.servertype.lower()
+    selected_modloader = "vanilla"
+    version = "1.21.10"
 
     match selected_modloader:
         case "vanilla":
-            vanilla_download(args.versions)
-        case "fabric":
-            fabric_download(args.versions)
-        case "paper":
-            paper_download(args.versions)
-        case "purpur":
-            purpur_download(args.versions)
-        case "neoforge":
-            neoforge_download(args.versions)
-        case "forge":
-            forge_download(args.versions)
+            vanilla_download(version)
+        # case "fabric":
+        #     fabric_download(version)
+        # case "paper":
+        #     paper_download(version)
+        # case "purpur":
+        #     purpur_download(version)
+        # case "neoforge":
+        #     neoforge_download(version)
+        # case "forge":
+        #     forge_download(version)
         case _:
             print("Invalid Modloader / Server Software")
 
