5dd98f5e296e51a6400e499ab4c67f22b1eb0336
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2025-10-30 10:33:08 -0400

    Refactor neoforge into main

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import json
 import os
 import shutil
+import xml.etree.ElementTree as ET
+import subprocess
 
 def vanilla_download(version):
     print("Downloading Vanilla " + version)
@@ -185,6 +187,110 @@ def purpur_download(version):
         print(f"Error copying file: {e}")
         return None
 
+def neoforge_download(version):
+    print("Downloading NeoForge Maven Data")
+    # NeoForge has a helpful XML document that contains the names of all the NeoForge versions.
+    maven_url = "https://maven.neoforged.net/releases/net/neoforged/neoforge/maven-metadata.xml"
+    versions_list = []
+
+    try:
+        response = requests.get(maven_url, stream=True)
+        response.raise_for_status()
+
+        maven_xml = response.text
+
+        print("NeoForge Maven Data Downloaded Successfully")
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading NeoForge Maven Data: {e}")
+        return None
+
+    root = ET.fromstring(maven_xml)
+
+    # Parse the XML document to find all versions and then add them to a list containing all versions correlating to the selected Minecraft version
+    for versionMeta in root.findall(".//versions"):
+        for metaVersions in versionMeta:
+            versions_list.append(metaVersions.text)
+
+    split_version = version.split('.')
+    # NeoForge versions have a consistent naming scheme. They begin with the minor version in Minecraft, then have the patch number, then the NeoForge patch number.
+    # Some examples of this would be all Minecraft 1.21.1 NeoForge versions starting with 21.1. and all 1.20.4 versions starting with 20.4.
+    # This consistent naming scheme is used to find all NeoForge versions compatible with our selected Minecraft version, and then select the latest one.
+    version_name_starter = f"{split_version[1]}.{split_version[2]}"
+
+    good_version_list = []
+    for neo_version in versions_list:
+        if neo_version.startswith(version_name_starter):
+            good_version_list.append(neo_version)
+
+    if not good_version_list:
+        print(f"Version {version} not found in NeoForge Maven Data")
+        return None
+
+    version_download_url = f"https://maven.neoforged.net/releases/net/neoforged/neoforge/{good_version_list[-1]}/neoforge-{good_version_list[-1]}-installer.jar"
+    output_dir = "/"
+    output_file = os.path.join(output_dir, "installer.jar")
+    os.makedirs(output_dir, exist_ok=True)
+
+    try:
+        response = requests.get(version_download_url, stream=True)
+        response.raise_for_status()
+
+        print("Downloading NeoForge " + version + " Installer file")
+        with open(output_file, "wb") as file:
+            for chunk in response.iter_content(8192):
+                file.write(chunk)
+        print("Version installer downloaded successfully to " + output_dir)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading version installer: {e}")
+        return None
+
+    if not os.path.isdir(output_dir):
+        print(f"Error: Directory {output_dir} does not exist")
+        return None
+    else:
+        try:
+            print(f"Installing NeoForge {version} server... (May take a few moments)")
+            result = subprocess.run(["java", "-jar", "/installer.jar", "--installServer"], cwd=output_dir, capture_output=True, text=True, check=True)
+            print(f"NeoForge {version} server installed successfully")
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing command: {e}")
+            print(f"Stderr: {e.stderr}")
+            return None
+
+    print("Cleaning NeoForge installed files")
+    for item_name in os.listdir(output_dir):
+        item_path = os.path.join(output_dir, item_name)
+        if os.path.isfile(item_path):
+            os.remove(item_path)
+    print("Cleaned NeoForge installed files successfully")
+
+    try:
+        response = requests.get("https://github.com/neoforged/ServerStarterJar/releases/download/0.1.34/server.jar", stream=True)
+        response.raise_for_status()
+
+        with open("/server.jar", "wb") as file:
+            for chunk in response.iter_content(8192):
+                file.write(chunk)
+        print(f"Finished downloading server starter jar")
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading version installer: {e}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+    
+    print("Creating EULA file")
+    eula_dir = os.path.join(output_dir, "eula.txt")
+    with open(eula_dir, "w") as file:
+        file.write("eula=true")
+    print("Created EULA file successfully")
+
+    print("Zipping files")
+    shutil.make_archive(f"{good_version_list[-1]}", "zip", output_dir)
+    shutil.move(f"{good_version_list[-1]}.zip", output_dir)
+    print("Finished zipping files")
+
 def main():
     selected_modloader = "purpur"
     version = "1.21.10"
@@ -198,8 +304,8 @@ def main():
             paper_download(version)
         case "purpur":
             purpur_download(version)
-        # case "neoforge":
-        #     neoforge_download(version)
+        case "neoforge":
+            neoforge_download(version)
         # case "forge":
         #     forge_download(version)
         case _:
